f9fc38871016520d210b0a7ee71510577e91dd75
kahmedovp-byte <kahmedovp@gmail.com>
2026-02-12 11:33:22 +0700

    thbgrvf

--- LR1.2.py
+++ LR1.2.py
@@ -2,22 +2,29 @@ import re
 from datetime import date
 
 class MeterReading:
-    def __init__(self, resource_type: str, reading_date: date, value: float):
+    def __init__(self, resource_type: str, reading_date: date, value: float, color: str):
         self.resource_type = resource_type
+        self.color = color
         self.reading_date = reading_date
         self.value = value
+
 
     def __str__(self):
         return (
             f"Тип ресурса: {self.resource_type}\n"
+            f"Цвет: {self.color} \n"
             f"Дата: {self.reading_date}\n"
             f"Значение: {self.value}"
+
         )
 
 
 def parse_meter_reading(input_string: str) -> MeterReading:
-    resource_match = re.search(r'"([^"]+)"', input_string)
-    resource_type = resource_match.group(1)
+
+    string_matches = re.findall(r'"([^"]+)"', input_string)
+
+    resource_type = string_matches[0]
+    color_type = string_matches[1]
 
     date_match = re.search(r'(\d{4})\.(\d{2})\.(\d{2})', input_string)
     year, month, day = map(int, date_match.groups())
@@ -26,15 +33,19 @@ def parse_meter_reading(input_string: str) -> MeterReading:
     value_matches = re.findall(r'\d+\.\d+', input_string)
     value = float(value_matches[1])
 
-    return MeterReading(resource_type, reading_date, value)
+
+    return MeterReading(resource_type, reading_date, value, color_type)
 
 
 if __name__ == "__main__":
     print("Введите строку с показаниями счетчиков:")
-    print('Пример: ПоказанияСчетчиков "Электроэнергия" 2026.02.07 1234.56')
+    print('Пример: ПоказанияСчетчиков "Электроэнергия" "красный" 2026.02.07 1234.56')
 
     input_line = input()
 
     reading = parse_meter_reading(input_line)
     print("\nРезультат обработки:\n")
     print(reading)
+
+
+
